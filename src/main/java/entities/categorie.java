1068900f54c30b9edb26e620ad5c3833e49ebb90
yassinehassine <yassinehassine16@gmail.com>
2024-02-16 00:06:14 -0800

    crud avec jointure

---
+++ src/main/java/entities/categorie.java
@@ -0,0 +1,53 @@
+package entities;
+
+public class categorie {
+    private  int id;
+    private type type;
+    private String description;
+
+
+
+    public categorie(int id, type type, String description) {
+        this.id = id;
+        this.type = type;
+        this.description = description;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public categorie setId(int id) {
+        this.id = id;
+        return null;
+
+    }
+
+    public type getType() {
+        return type;
+    }
+
+    public categorie setType(type type) {
+        this.type = type;
+        return null;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+    @Override
+    public String toString() {
+        return "categorie{" +
+                "id=" + id +
+                ", type=" + type +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/entities/produit.java
@@ -0,0 +1,118 @@
+package entities;
+ import entities.categorie;
+public class produit {
+   private int id;
+   private String nom;
+   private String image;
+   private float prix;
+   private int quantite;
+   private String description;
+  private static categorie categorie;
+  private   int id_bilan_financier;
+   private int id_admin;
+
+
+
+
+
+    public produit() {
+    }
+
+
+    public produit(int id, String nom, String image, float prix, int quantite, String description, categorie categorie, int id_bilan_financier, int id_admin) {
+        this.id = id;
+        this.nom = nom;
+        this.image = image;
+        this.prix = prix;
+        this.quantite = quantite;
+        this.description = description;
+        this.categorie = categorie;
+        this.id_bilan_financier = id_bilan_financier;
+        this.id_admin = id_admin;
+    }
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public float getPrix() {
+        return prix;
+    }
+
+    public int getQuantite() {
+        return quantite;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public int getId_bilan_financier() {
+        return id_bilan_financier;
+    }
+    public int getId_admin() {
+        return id_admin;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public void setPrix(float prix) {
+        this.prix = prix;
+    }
+
+    public void setQuantite(int quantite) {
+        this.quantite = quantite;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public void setId_bilan_financier(int id_bilan_financier) {
+        this.id_bilan_financier = id_bilan_financier;
+    }
+    public void setId_admin(int id_admin) {
+        this.id_admin = id_admin;
+    }
+
+    public  categorie getCategorie() {
+        return categorie;
+    }
+
+
+    public void setCategorie(categorie categorie) {
+        this.categorie = categorie;
+    }
+
+    @Override
+    public String toString() {
+        return "produit{" +
+                "id=" + id +
+                ", nom='" + nom + '\'' +
+                ", image='" + image + '\'' +
+                ", prix=" + prix +
+                ", quantite=" + quantite +
+                ", description='" + description + '\'' +
+                ", categorie=" + categorie +
+                ", id_bilan_financier=" + id_bilan_financier +
+                ", id_admin=" + id_admin +
+                '}';
+    }
+}
