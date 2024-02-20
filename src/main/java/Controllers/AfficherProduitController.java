82125f495123370ad53700fe5d8dd5ef3ca89766
yassinehassine <yassinehassine16@gmail.com>
2024-02-19 23:00:42 -0800

    crud javafx dans la meme interface

--- src/main/java/Controllers/AfficherProduitController.java
+++ src/main/java/Controllers/AfficherProduitController.java
@@ -1,6 +1,8 @@
 package Controllers;
 
+import entities.categorie;
 import entities.produit;
+import javafx.event.ActionEvent;
 import service.produitService;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -32,19 +34,49 @@ public class AfficherProduitController implements Initializable {
 
     @FXML
     private TableColumn<produit, String> descriptionColumn;
+
     @FXML
     private TableColumn<produit, String> categorieColumn;
+
     @FXML
     private TableColumn<produit, String> idbilanfinancierColumn;
+
     @FXML
     private TableColumn<produit, String> idadminColumn;
 
-    private final ObservableList<produit> produits = FXCollections.observableArrayList();
     @FXML
     private TableColumn<produit, Void> deleteColumn;
 
-    private produitService produitService;
+    @FXML
+    private TableColumn<produit, Void> selectColumn;
+    @FXML
+    private TextField txtId;
 
+    @FXML
+    private TextField txtnom;
+
+    @FXML
+    private TextField txtimage;
+
+    @FXML
+    private TextField txtprix;
+
+    @FXML
+    private TextField txtquantite;
+
+    @FXML
+    private TextField txtdescription;
+
+    @FXML
+    private TextField txtcategorie;
+
+    @FXML
+    private TextField txtid_bilan_financier;
+
+    @FXML
+    private TextField txtid_admin;
+
+    private produitService produitService;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -55,8 +87,8 @@ public class AfficherProduitController implements Initializable {
         nomColumn.setCellValueFactory(new PropertyValueFactory<>("nom"));
         prixColumn.setCellValueFactory(new PropertyValueFactory<>("prix"));
         quantiteColumn.setCellValueFactory(new PropertyValueFactory<>("quantite"));
-       descriptionColumn.setCellValueFactory(new PropertyValueFactory<>("description"));
-       categorieColumn.setCellValueFactory(new PropertyValueFactory<>("categorie"));
+        descriptionColumn.setCellValueFactory(new PropertyValueFactory<>("description"));
+        categorieColumn.setCellValueFactory(new PropertyValueFactory<>("categorie"));
         idbilanfinancierColumn.setCellValueFactory(new PropertyValueFactory<>("id_bilan_financier"));
         idadminColumn.setCellValueFactory(new PropertyValueFactory<>("id_admin"));
 
@@ -67,7 +99,7 @@ public class AfficherProduitController implements Initializable {
             {
                 deleteButton.setOnAction(event -> {
                     produit produit = getTableView().getItems().get(getIndex());
-                    supprimerEvenement(produit.getId());
+                    supprimerproduit(produit.getId());
                 });
             }
 
@@ -82,14 +114,42 @@ public class AfficherProduitController implements Initializable {
             }
         });
 
+        // Create the "Sélectionner" button column
+        selectColumn.setCellFactory(param -> new TableCell<>() {
+            private final Button selectButton = new Button("Sélectionner");
+
+            {
+                selectButton.setOnAction(event -> {
+                    produit produit = getTableView().getItems().get(getIndex());
+                    txtId.setText(String.valueOf(produit.getId()));
+                    txtnom.setText(produit.getNom());
+                    txtimage.setText(produit.getImage());
+                    txtprix.setText(String.valueOf(produit.getPrix()));
+                    txtquantite.setText(String.valueOf(produit.getQuantite()));
+                    txtdescription.setText(produit.getDescription());
+                    int categoryId = produit.getCategorie().getId();
+                    txtcategorie.setText(String.valueOf(categoryId));
+                    txtid_bilan_financier.setText(String.valueOf(produit.getId_bilan_financier()));
+                    txtid_admin.setText(String.valueOf(produit.getId_admin()));
+                });
+            }
+
+            @Override
+            protected void updateItem(Void item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty) {
+                    setGraphic(null);
+                } else {
+                    setGraphic(selectButton);
+                }
+            }
+        });
+
         // Load data from the database
-        List<produit> produitList =produitService.readAll();
-        produits.addAll(produitList);
-        tableViewProduit.setItems(produits);
+        refreshTable();
     }
 
-
-    public void supprimerEvenement(int id) {
+    public void supprimerproduit(int id) {
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
         alert.setTitle("Confirmation");
         alert.setHeaderText(null);
@@ -104,8 +164,84 @@ public class AfficherProduitController implements Initializable {
     }
 
     private void refreshTable() {
-        produits.clear();
-        List<produit> evenementsList = produitService.readAll();
-        produits.addAll(evenementsList);
-        tableViewProduit.setItems(produits);
-    }}
+        tableViewProduit.getItems().clear();
+        tableViewProduit.getItems().addAll(produitService.readAll());
+    }
+
+    @FXML
+    public void updateProduit(ActionEvent event) {
+        // Get the selected product
+        produit selectedProduit = tableViewProduit.getSelectionModel().getSelectedItem();
+      /*  if (selectedProduit == null) {
+            showAlert(Alert.AlertType.ERROR, "Erreur", "Aucun produit sélectionné", "Veuillez sélectionner un produit à mettre à jour.");
+            return;
+        }*/
+
+        // Get data from text fields
+        int id = Integer.parseInt(txtId.getText());
+        String nom = txtnom.getText();
+        String image = txtimage.getText();
+        float prix = Float.parseFloat(txtprix.getText());
+        int quantite = Integer.parseInt(txtquantite.getText());
+        String description = txtdescription.getText();
+        int categoryId = Integer.parseInt(txtcategorie.getText());
+        categorie category = new categorie(categoryId,null,description);
+        int id_bilan_financier = Integer.parseInt(txtid_bilan_financier.getText());
+        int id_admin = Integer.parseInt(txtid_admin.getText());
+        produit p = new produit(nom, image, prix, quantite, description, category, id_bilan_financier, id_admin);
+
+        // Create a new produit object with updated data
+        produit updatedProduit = new produit(nom, image, prix, quantite, description, category, id_bilan_financier, id_admin);
+
+        // Update the produit in the database
+        produitService.update(id, updatedProduit);
+        txtId.setText("");
+        clearFields();
+
+        // Refresh the table
+        refreshTable();
+    }
+
+    @FXML
+    void addProduit() {
+        // Get data from text fields
+        String nom = txtnom.getText();
+        String image = txtimage.getText();
+        float prix = Float.parseFloat(txtprix.getText());
+        int quantite = Integer.parseInt(txtquantite.getText());
+        String description = txtdescription.getText();
+        int categoryId = Integer.parseInt(txtcategorie.getText());
+        categorie category = new categorie(categoryId, null, description);
+        int id_bilan_financier = Integer.parseInt(txtid_bilan_financier.getText());
+        int id_admin = Integer.parseInt(txtid_admin.getText());
+
+        // Create a new produit object
+        produit p = new produit(nom, image, prix, quantite, description, category, id_bilan_financier, id_admin);
+
+        // Add the produit to the database
+        produitService.add(p);
+
+        clearFields();
+
+        // Refresh the table
+        refreshTable();
+    }
+
+    private void showAlert(Alert.AlertType alertType, String title, String headerText, String contentText) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(headerText);
+        alert.setContentText(contentText);
+        alert.showAndWait();
+    }
+    private void clearFields() {
+        txtnom.setText("");
+        txtimage.setText("");
+        txtprix.setText("");
+        txtquantite.setText("");
+        txtdescription.setText("");
+        txtcategorie.setText("");
+        txtid_bilan_financier.setText("");
+        txtid_admin.setText("");
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -16,14 +16,14 @@ import service.produitService;
 
 public class Main {
     public static void main(String[] args) {
-       DataSource ds1 = DataSource.getInstance();
+      /* DataSource ds1 = DataSource.getInstance();
         System.out.println(ds1);
        produitService ps= new produitService();
         categorieService pc= new categorieService();
         categorie c1 = new categorie(5,vestimentaire,"ffff");
         categorie c2= new categorie(6,vestimentaire,"ffff");
         produit p2 = new produit("prot","image.png",50.3F,5,"555555",c2,1,1);
-       // ps.add(p2);
+       ps.add(p2);*/
       //  pc.add(c2);
        // ps.delete(2);
 
@@ -38,7 +38,7 @@ public class Main {
 
 //ps.delete(2);
 
-        ps.readAll().forEach(System.out::println);
+     //   ps.readAll().forEach(System.out::println);
       /*  p1.setDescription("lyaass");
         ps.update(1,p1);*/
         }
