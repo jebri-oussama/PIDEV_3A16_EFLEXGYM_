a3879bb43fe00c23327771686a121a69702ac3be
nadazaghdoud <nadazaghdoud.5@gmail.com>
2024-02-23 13:07:42 +0100

    a

---
+++ src/main/java/GestionFinance/controller/BilanFinancierController.java
@@ -0,0 +1,8 @@
+package GestionFinance.controller;
+
+import javafx.event.ActionEvent;
+
+public class BilanFinancierController {
+    public void calculerProfit(ActionEvent actionEvent) {
+    }
+}
